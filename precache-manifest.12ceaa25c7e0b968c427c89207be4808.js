8432fffdc1b75567ee6a9313b4bc51c4ea970c61
RzChan <big556613@gmail.com>
2020-08-04 22:50:01 +0800

    Barotrauma Dev

---
+++ precache-manifest.12ceaa25c7e0b968c427c89207be4808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ff9382717c0481bbf0a6286925ecb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc2a2709c160717c2332",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2d2ffd2407db511e79f0",
+    "url": "/static/css/main.8ee05591.chunk.css"
+  },
+  {
+    "revision": "cc2a2709c160717c2332",
+    "url": "/static/js/2.01381add.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.01381add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2ffd2407db511e79f0",
+    "url": "/static/js/main.38738682.chunk.js"
+  },
+  {
+    "revision": "29f0f10ee10748617b3a",
+    "url": "/static/js/runtime-main.e1f49279.js"
+  }
+]);
