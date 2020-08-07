c56049f59260cdecd7a9b6ad90ab808e2b869f8d
RzChan <big556613@gmail.com>
2020-08-08 01:00:46 +0800

    Add weapons, crafting time, requrie skill.

---
+++ precache-manifest.711c1819562cc315da8acf26e8b3ef8a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d989a495fc93a21147cd7bca70d29cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae05214f32ad335f886",
+    "url": "/static/css/2.620ed6e8.chunk.css"
+  },
+  {
+    "revision": "208001d83b8caced61f9",
+    "url": "/static/css/main.ac077d00.chunk.css"
+  },
+  {
+    "revision": "7ae05214f32ad335f886",
+    "url": "/static/js/2.0f4c7485.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0f4c7485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208001d83b8caced61f9",
+    "url": "/static/js/main.4c919ed2.chunk.js"
+  },
+  {
+    "revision": "29f0f10ee10748617b3a",
+    "url": "/static/js/runtime-main.e1f49279.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d075ef7f2003da3a8565618f6bae609.js"
+  "/precache-manifest.711c1819562cc315da8acf26e8b3ef8a.js"
 );
 
 self.addEventListener('message', (event) => {
